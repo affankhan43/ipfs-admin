0db47e1b699d33ba246215fae232f10adb936aa0
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-08 00:01:25 +0500

    login

--- index.php
+++ index.php
@@ -92,8 +92,6 @@ ini_set("display_errors", 1);
         </div>
       </div>
       <div class="copyrights text-center">
-        <p>Design by <a href="https://bootstrapious.com/admin-templates" class="external">Bootstrapious</a>
-          <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
         </p>
       </div>
     </div>
