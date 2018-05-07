76867c5e3c01aa5d8ec1f61a62cb1fc954a4ccf1
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 12:25:57 +0500

    form builder

--- upload.php
+++ upload.php
@@ -3,11 +3,9 @@
 // if (isset($_POST['action']) && $_POST['action'] == 'saveForm'){\
 //
 //   $formdata = $_POST['formdata'];
- if($_SERVER["REQUEST_METHOD"] == "POST") {
-   formData = $_POST['formData'];
+    formData = $_POST['formData'];
 
-   echo "success";
+   echo formData;
 
- }
 
 // }
