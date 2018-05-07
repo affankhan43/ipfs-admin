af2d4df161832060cd53849ef9e84f0184aa4682
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 11:53:10 +0500

    form builder

--- tables.php
+++ tables.php
@@ -147,7 +147,7 @@
             <div class="container-fluid">
               <div class="row">
 
-                <iframe src="formbuilder.html" style="width: 100%; height: auto;"></iframe>
+                <iframe src="formbuilder.html" style="width: 100%; height: 500px;"></iframe>
 
               </div>
             </div>
