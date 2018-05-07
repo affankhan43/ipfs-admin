42bb1942bc867f401a925d86c062a833d545646d
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 13:03:15 +0500

    form builder

--- upload.php
+++ upload.php
@@ -1,6 +1,8 @@
 <?php
 include 'config.php';
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
     if(isset($_POST['action']) && $_POST['action'] == 'saveForm'){
       $formData = preg_replace('/\s+/', '', $_POST['formdata']);
       $sql = "UPDATE form_builder SET form_fields = '$formData' WHERE id = '1' ";
