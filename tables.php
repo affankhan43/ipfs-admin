05c9257f71a826e6df9186d9691a2618f12df0b5
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 11:45:08 +0500

    form builder

--- tables.php
+++ tables.php
@@ -151,7 +151,48 @@
           </div>
           <section class="tables">
             <div class="container-fluid">
-              <div class="row"></div>
+              <div class="row">
+
+                <div class="content">
+                  <h1>jQuery formBuilder</h1>
+                  <div id="stage1" class="build-wrap"></div>
+                  <form class="render-wrap"></form>
+                  <button id="edit-form">Edit Form</button>
+                  <div class="action-buttons" style="display:none;">
+                    <button id="showData" type="button">Show Data</button>
+                    <button id="clearFields" type="button">Clear All Fields</button>
+                    <button id="getData" type="button">Get Data</button>
+                    <button id="getXML" type="button">Get XML Data</button>
+                    <button id="getJSON" type="button">Get JSON Data</button>
+                    <button id="getJS" type="button">Get JS Data</button>
+                    <button id="setData" type="button">Set Data</button>
+                    <button id="addField" type="button">Add Field</button>
+                    <button id="removeField" type="button">Remove Field</button>
+                    <button id="testSubmit" type="submit">Test Submit</button>
+                    <button id="resetDemo" type="button">Reset Demo</button>
+                    <h2>i18n</h2>
+                    <select id="setLanguage">
+                      <option value="ar-TN" dir="rtl">تونسي</option>
+                      <option value="de-DE">Deutsch</option>
+                      <option value="en-US">English</option>
+                      <option value="es-ES">español</option>
+                      <option value="fa-IR" dir="rtl">فارسى</option>
+                      <option value="fr-FR">français</option>
+                      <option value="nb-NO">norsk</option>
+                      <option value="nl-NL">Nederlands</option>
+                      <option value="pt-BR">português</option>
+                      <option value="ro-RO">român</option>
+                      <option value="ru-RU">Русский язык</option>
+                      <option value="tr-TR">Türkçe</option>
+                      <option value="vi-VN">tiếng việt</option>
+                      <option value="zh-TW">台語</option>
+                    </select>
+                  </div>
+                  <br/>
+                  <button onclick="saveForm()" class="btn btn-lg btn-success">Save Form</button>
+                </div>
+
+              </div>
             </div>
           </section>
           <!-- Page Footer-->
