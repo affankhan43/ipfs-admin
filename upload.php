e75bf6d014151aa19260401d17068c417e8b3fc0
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 12:39:48 +0500

    form builder

--- upload.php
+++ upload.php
@@ -1,10 +1,7 @@
 <?php
+include 'config.php';
 
-// if (isset($_POST['action']) && $_POST['action'] == 'saveForm'){\
-//
-//   $formdata = $_POST['formdata'];
     $formData = preg_replace('/\s+/', '', $_POST['formdata']);
-    echo $formData;
-
-
-// }
+    $sql = "UPDATE form_builder SET form_fields = '$formData' WHERE id = '1' ";
+    $query = mysqli_query($db, $sql);
+    echo 'success';
