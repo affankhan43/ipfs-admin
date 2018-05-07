b396ccf7ae7aaf7eeaae2de16d5f0ef9a7a3e887
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 11:45:55 +0500

    form builder

--- tables.php
+++ tables.php
@@ -19,12 +19,6 @@
     <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="css/custom.css">
-
-    <link rel="stylesheet" type="text/css" href="formbuilder/css/demo.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.1/jquery.rateyo.min.css">
-    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-
     <!-- Favicon-->
     <link rel="shortcut icon" href="img/favicon.ico">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
@@ -130,7 +124,7 @@
           </div>
           <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
           <ul class="list-unstyled">
-                    <li><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+                    <li ><a href="index.html"> <i class="icon-home"></i>Home </a></li>
                     <li class="active"><a href="tables.php"> <i class="icon-grid"></i>Form Builder </a></li>
                     <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Set Fees </a></li>
           </ul>
@@ -151,48 +145,7 @@
           </div>
           <section class="tables">
             <div class="container-fluid">
-              <div class="row">
-
-                <div class="content">
-                  <h1>jQuery formBuilder</h1>
-                  <div id="stage1" class="build-wrap"></div>
-                  <form class="render-wrap"></form>
-                  <button id="edit-form">Edit Form</button>
-                  <div class="action-buttons" style="display:none;">
-                    <button id="showData" type="button">Show Data</button>
-                    <button id="clearFields" type="button">Clear All Fields</button>
-                    <button id="getData" type="button">Get Data</button>
-                    <button id="getXML" type="button">Get XML Data</button>
-                    <button id="getJSON" type="button">Get JSON Data</button>
-                    <button id="getJS" type="button">Get JS Data</button>
-                    <button id="setData" type="button">Set Data</button>
-                    <button id="addField" type="button">Add Field</button>
-                    <button id="removeField" type="button">Remove Field</button>
-                    <button id="testSubmit" type="submit">Test Submit</button>
-                    <button id="resetDemo" type="button">Reset Demo</button>
-                    <h2>i18n</h2>
-                    <select id="setLanguage">
-                      <option value="ar-TN" dir="rtl">تونسي</option>
-                      <option value="de-DE">Deutsch</option>
-                      <option value="en-US">English</option>
-                      <option value="es-ES">español</option>
-                      <option value="fa-IR" dir="rtl">فارسى</option>
-                      <option value="fr-FR">français</option>
-                      <option value="nb-NO">norsk</option>
-                      <option value="nl-NL">Nederlands</option>
-                      <option value="pt-BR">português</option>
-                      <option value="ro-RO">român</option>
-                      <option value="ru-RU">Русский язык</option>
-                      <option value="tr-TR">Türkçe</option>
-                      <option value="vi-VN">tiếng việt</option>
-                      <option value="zh-TW">台語</option>
-                    </select>
-                  </div>
-                  <br/>
-                  <button onclick="saveForm()" class="btn btn-lg btn-success">Save Form</button>
-                </div>
-
-              </div>
+              <div class="row"></div>
             </div>
           </section>
           <!-- Page Footer-->
@@ -212,19 +165,6 @@
         </div>
       </div>
     </div>
-    <script type="text/javascript">
-      var formBuilder;
-    </script>
-    <script type="text/javascript">
-      function saveForm(){
-        console.log(formBuilder.actions.getData('xml'));
-      }
-    </script>
-    <script src="formbuilder/js/vendor.js"></script>
-    <script src="formbuilder/js/form-builder.min.js"></script>
-    <script src="formbuilder/js/form-render.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.1/jquery.rateyo.min.js"></script>
-    <script src="formbuilder/js/demo.js"></script>
     <!-- JavaScript files-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/popper.js/umd/popper.min.js"> </script>
