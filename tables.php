99af6b1a6430437a9c633a37a3dbeaaf8631df16
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 11:39:58 +0500

    form builder

--- tables.php
+++ tables.php
@@ -124,8 +124,8 @@
           </div>
           <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
           <ul class="list-unstyled">
-                    <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
-                    <li><a href="tables.php"> <i class="icon-grid"></i>Form Builder </a></li>
+                    <li><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+                    <li class="active"><a href="tables.php"> <i class="icon-grid"></i>Form Builder </a></li>
                     <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Set Fees </a></li>
           </ul>
         </nav>
